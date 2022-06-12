14d422d6da56be847c3900d6cd8059d083b72a34
Arham Chowdhury <arham.chowdhury@paytm.com>
2022-06-12 20:53:47 +0530

    mutiple pointer
    return unique values using mutiple pointer approach

--- js/multiple-pointer.js
+++ js/multiple-pointer.js
@@ -30,3 +30,36 @@ const sumZero1 = (a) => {
   return false;
 };
 console.log(sumZero1([-7, -4, -3, -1, 0, 1, 2, 3, 4, 6]));
+
+// count unique values in a array
+const uniqueValues = (a) => {
+  if (a.length === 0) {
+    return 0;
+  }
+
+  if (a.length === 1) {
+    return a[0];
+  }
+
+  // start from frist
+  let pos = 0;
+  let unique = [a[0]];
+
+  // loop over array using while loop or for loop
+  while (pos < a.length - 1) {
+    // check if number and its succeeding number is same or not
+    if (a[pos] !== a[pos + 1]) {
+      // if number and succeeding number is not same, push 2nd item to unique
+      unique.push(a[pos + 1]);
+    }
+    pos++;
+    // increment pos
+  }
+  // return unique number
+  return unique;
+};
+
+console.log(uniqueValues([0, 1, 1, 2, 3, 4, 5, 5, 5, 5])); // [0, 1, 2, 3, 4, 5]
+console.log(uniqueValues([])); // 0
+console.log(uniqueValues([1])); // 1
+console.log(uniqueValues([1, 1, 1, 1, 1, 2])); // [1, 2]
