4f913b8ba7859951c7a2a921a53e1e5704bf41fd
Arham Chowdhry <arham.chowdhry@abbott.com>
2022-09-26 20:57:38 +0530

    multiple-pointer to calcualte unique value

--- js/multiple-pointer.js
+++ js/multiple-pointer.js
@@ -40,6 +40,7 @@ var sumZero1 = (a) => {
 
 // console.log(sumzero([-7, -4, -3, -1, 0, 1, 2, 3, 4, 6]));
 console.log(sumZero1([-4, -3, -2, -1, 0, 1, 2, 5]));
+
 // count unique values in a array
 const uniqueValues = (a) => {
   if (a.length === 0) {
@@ -72,3 +73,20 @@ console.log(uniqueValues([0, 1, 1, 2, 3, 4, 5, 5, 5, 5])); // [0, 1, 2, 3, 4, 5]
 console.log(uniqueValues([])); // 0
 console.log(uniqueValues([1])); // 1
 console.log(uniqueValues([1, 1, 1, 1, 1, 2])); // [1, 2]
+
+const countUniqueVal = (a1) => {
+  // define a unique item, define count
+  let uniqueItem;
+  let count = 0;
+  // loop over array
+  for (let item of a1) {
+    // if item inside array !== unique item count++
+    if (item !== uniqueItem) {
+      uniqueItem = item;
+      count++;
+    }
+  }
+  return count;
+};
+
+console.log(countUniqueVal([1, 2, 2, 3, 3, 3, 3, 3, 4, 4, 4, 4, 5, 100, 200]));
